0737649cb7aab6ef77ffb0eaf49149d0076e66c5
Seetha Devi <seethadevi213@gmail.com>
2026-02-19 01:08:34 +0530

    Update Hooks.cs

--- StepDefinitions/StepDefinitions/Hooks.cs
+++ StepDefinitions/StepDefinitions/Hooks.cs
@@ -1,30 +1,39 @@
 using OpenQA.Selenium;
 using TechTalk.SpecFlow;
 using SeleniumCSharpBDDFramework.Drivers;
+using SeleniumCSharpBDDFramework.Utilities;
 
 namespace SeleniumCSharpBDDFramework.StepDefinitions
 {
     [Binding]
-    public class Hooks
-    {
-        public IWebDriver Driver { get; private set; }
-        private DriverFactory _driverFactory;
+public class Hooks
+{
+    public IWebDriver Driver { get; private set; }
+    private DriverFactory _driverFactory;
+    private readonly ScenarioContext _scenarioContext;
 
-        [BeforeScenario]
-        public void Setup()
+    public Hooks(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [BeforeScenario]
+    public void Setup()
+    {
+        _driverFactory = new DriverFactory();
+        Driver = _driverFactory.InitDriver();
+    }
+
+    [AfterScenario]
+    public void TearDown()
+    {
+        if (_scenarioContext.TestError != null)
         {
-            _driverFactory = new DriverFactory();
-            Driver = _driverFactory.InitDriver();
+            ScreenshotHelper.CaptureScreenshot(Driver, _scenarioContext.ScenarioInfo.Title);
         }
 
-        [AfterScenario]
-        public void TearDown()
-        {
-            if (Driver != null)
-            {
-                Driver.Quit();
-                Driver.Dispose();
-            }
-            }
+        Driver?.Quit();
     }
+}
+
 }
